44f3cba63abff7ab8b0ffd348fb8a877d41df839
LengJunT <lengroubao@163.com>
2023-05-18 22:31:43 +0800

    feat: ui更新

--- packages/tkit-im/src/im/compontents/Contacts/index.tsx
+++ packages/tkit-im/src/im/compontents/Contacts/index.tsx
@@ -7,8 +7,9 @@ const data = [
     {
         id: 0,
         avatar: 'https://randomuser.me/api/portraits/women/42.jpg',
-        label: '用户',
-        lastMsg: '哈哈哈'
+        label: '用户2444545',
+        lastMsg: '哈哈哈',
+        time: '下午2:15',
     }
 ]
 
@@ -16,15 +17,16 @@ for (let i = 1; i < 20; i++) {
     data.push({
         id: i,
         avatar: i % 2 == 0 ? 'https://randomuser.me/api/portraits/women/42.jpg' : 'https://randomuser.me/api/portraits/women/40.jpg',
-        label: '用户' + i,
-        lastMsg: '哈哈哈'
+        label: '用户用户用户' + i,
+        lastMsg: '哈哈哈',
+        time: '昨天',
     })
 }
 
 
 const Contacts = () => {
 
-  return <div className={prefixClassNames('contacts', 'bg-white')}
+  return <div className={prefixClassNames('contacts', 'bg-white overflow-auto')}
     style={{ width: '200px'}}
   >
       <List
@@ -33,7 +35,10 @@ const Contacts = () => {
               return <List.Item key={item.id}>
                   <List.Item.Meta
                       avatar={<Avatar size="large" src={item.avatar} />}
-                      title={item.label}
+                      title={<>
+                        <span className={'msg-user-name'}>{item.label}</span>
+                        <span className={'msg-time pl-1'}>{item.time}</span>
+                      </>}
                       description={item.lastMsg}
                   />
               </List.Item>
